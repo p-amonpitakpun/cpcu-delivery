e1dee9e9abfd0d9cf65b0b0f332d210d792a3080
Pawat Amonpitakpun <p.amonpitakpun@gmail.com>
2021-02-07 22:43:35 +0700

    New main procedure

--- src/graph_mapping/scripts/mapping.py
+++ src/graph_mapping/scripts/mapping.py
@@ -8,44 +8,32 @@ from std_msgs.msg import String, Float32MultiArray
 from sensor_msgs.msg import Image, LaserScan
 from cv_bridge import CvBridge
 
+from .modules.graph_slam import GraphSLAM
 
-
-bridge = CvBridge()
-img = None
-
-def callback(image, laser_scanner):
-    print(image)
-
-def image_callback(data):
-    global img
-    try:
-        image = bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
-        img = image
-    except e:
-        print(e)
 
 def main():
-    cv2.namedWindow('image')
-    # rospy.init_node('mapping', anonymous=True)
+    rospy.init_node('mapping', anonymous=True)
 
-    # image_sub = message_filters.Subscriber('/image', Image)
-    # laser_scanner_sub = message_filters.Subscriber('laser_scanner', Float32MultiArray)
+    graph = GraphSLAM()
 
-    # time_sync = message_filters.TimeSynchronizer([image_sub, laser_scanner_sub], 1)
-    # time_sync.registerCallback(callback)
+    odometry_sub = message_filters.Subscriber('odometry', None)  # TODO
+    laser_scanner_sub = message_filters.Subscriber(
+        'laser_scanner', Float32MultiArray)
 
-    rospy.init_node('immage', anonymous=True)
+    time_sync = message_filters.TimeSynchronizer(
+        [odometry_sub, laser_scanner_sub], 1)
+    time_sync.registerCallback(graph.mapping)
 
-    rospy.Subscriber('/image', Image, image_callback)
+    cv2.namedWindow('graph')
 
     while not rospy.is_shutdown():
+        img = graph.getImage()
         if img is not None:
-            cv2.imshow('image', img)
+            cv2.imshow('graph', img)
             cv2.waitKey(3)
 
     rospy.spin()
 
 
 if __name__ == "__main__":
-    print('!')
     main()
