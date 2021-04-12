9c8f40cb2b5f5461758142bd93c361255f1c4a37
Pawat <p.amonpitakpun@gmail.com>
2021-04-12 15:36:07 +0700

    change scoring algo

--- src/localization/scripts/modules/PF.py
+++ src/localization/scripts/modules/PF.py
@@ -1,3 +1,4 @@
+import cv2
 import json
 import numpy as np
 
@@ -15,6 +16,8 @@ class ParticleFilter():
         self.rng = np.random.default_rng(RNG_SEED)
         self.last_update = None
         self.N = N
+        self.M = 8 * N
+        self.images = dict()
 
     def init(self, ref_map_path=None, ref_map_config_path=None, init_pose=np.array([0, 0, 0]), init_vel=np.array([0, 0, 0])):
         if ref_map_path is None or ref_map_config_path is None:
@@ -38,7 +41,7 @@ class ParticleFilter():
         self.particle_velocity = init_vel
 
         self.particles = None
-        self.scores = np.array([1] * self.N) / self.N
+        self.scores = np.array([1] * self.M) / self.M
 
     def update(self, transform, laser_scanner_data):
         now = datetime.now()
@@ -59,17 +62,52 @@ class ParticleFilter():
         self.particle = selected_particle
         self.last_update = now
 
+        if self.M / self.N > 1:
+            self.M = int(self.M - 100)
+
+    def getLoc(self):
+        now = datetime.now()
+        dt_s = (
+            now - self.last_update).total_seconds() if self.last_update is not None else 0
+        return self.particle + self.particle_velocity * dt_s
+
+    def getImage(self):
+        image_list = list(self.images.items())
+        N = len(image_list)
+        if N > 0:
+            ret_name, ret_image = image_list[0]
+            if N > 1:
+                shape = ret_image.shape
+                for name, image in image_list[1:]:
+                    ret_image = cv2.hconcat(
+                        ret_image, cv2.resize(image, shape))
+                    ret_name += ', ' + name
+            return ret_name, ret_image
+        else:
+            return None, None
+
     def sample(self):
 
         if self.particles is None:
-            self.particles = np.random.uniform(low=np.array([-2.5, -2.5, 0]), high=np.array([2.5, 2.5, np.pi * 2]), size=(self.N, 3))
+            samples = np.random.uniform(low=np.array([-2, -3]), high=np.array([2, 0]), size=(self.N, 2))
+            new_samples = []
+            for s in samples:
+                new_samples.append([s[0], s[1], 0])
+                new_samples.append([s[0], s[1], np.pi / 2])
+                new_samples.append([s[0], s[1], np.pi])
+                new_samples.append([s[0], s[1], np.pi * 3 / 2])
+                new_samples.append([s[0], s[1], np.pi / 4])
+                new_samples.append([s[0], s[1], np.pi * 3 / 4])
+                new_samples.append([s[0], s[1], np.pi * 5 / 4])
+                new_samples.append([s[0], s[1], np.pi * 7 / 4])
+            return np.array(new_samples)
 
         if len(self.scores) != len(self.particles):
             raise Exception(
                 f'Error: the length of scores ({len(self.scores)}) is not equal to the number of particles ({len(self.particles)}).')
 
         new_particles = self.rng.choice(
-            self.particles, size=self.N, p=self.scores)
+            self.particles, size=self.M, p=self.scores)
         return new_particles
 
     def predict(self, particles, transform):
@@ -78,24 +116,76 @@ class ParticleFilter():
                 f'Error: the shape of transformation {transform.shape} is not the same as the shape of particle {particles.shape}.')
 
         # cov_matrix = np.array([[1, 0, 1], [0, 1, 1], [1, 1, 1]]) * 1e-6
-        transform_err = 5e-3
+        transform_err = np.array([5e-3, 5e-3, 0.05])
 
-        predicted_particles = np.random.uniform(low=transform - transform_err,
-                                                high=transform + transform_err,
-                                                size=particles.shape)
+        predicted_particles = particles + np.random.uniform(low=transform - transform_err,
+                                                            high=transform + transform_err,
+                                                            size=particles.shape)
 
         return predicted_particles
 
     def get_scores(self, particles, laser_scanner_data):
         scores_length = len(particles)
-        scores = []
-        for i in range(scores_length):
-            score = np.random.rand()
-            scores.append(score)
-        return np.array(scores) / np.sum(scores)
+        scores = [0] * scores_length
+        # for i in range(scores_length):
+        #     score = np.random.rand()
+        #     scores.append(score)
 
-    def getLoc(self):
-        now = datetime.now()
-        dt_s = (
-            now - self.last_update).total_seconds() if self.last_update is not None else 0
-        return self.particle + self.particle_velocity * dt_s
+        reso = self.occGrid.resolution
+        y_shape, x_shape = self.occGrid.getShape()
+        x_offset, y_offset = x_shape // 2, y_shape // 2
+
+        particle_poses = []
+        particle_scans = []
+
+        for i, particle in enumerate(particles):
+            score = 0
+
+            X = particle
+            particle_poses.append(
+                [int(X[0] // reso + x_offset), int(X[1] // reso + y_offset)])
+
+            dx = particle
+            psi = dx[2]
+            R = np.array([[np.cos(psi), - np.sin(psi)],
+                          [np.sin(psi), np.cos(psi)]])
+            T = dx[0: 2].reshape((2, 1))
+
+            scan = []
+            for p in laser_scanner_data:
+                p = np.dot(p, R) + T.T
+                p = p.reshape((2,))
+                point = [int(p[0] // reso + x_offset),
+                         int(p[1] // reso + y_offset)]
+                if 0 <= point[0] < x_shape and 0 <= point[1] < y_shape:
+                    grid_score = self.occGrid.getOccupy(*point)
+                    score += grid_score if grid_score > 0 else 0
+                scan.append(point)
+
+            scores[i] = score if score > 0 else 0.00001
+            particle_scans.append(scan)
+
+        norm_scores = [1 / scores_length] * \
+            scores_length if np.sum(scores) == 0 else scores / np.sum(scores)
+
+        max_score = np.max(norm_scores)
+        particle_list = list(zip(particle_poses, particle_scans, norm_scores))
+        img = self.occGrid.getImage(-50, 50)
+        for pose, scan, score in particle_list:
+            if score == max_score:
+                pass
+            else:
+                img = cv2.circle(img, tuple(pose), 2, (0, 255, 0), -1)
+                for point in scan:
+                    img = cv2.circle(img, tuple(point), 2, (70, 70, 205), -1)
+        
+        idx = np.argmax(norm_scores)
+        img = cv2.circle(img, tuple(particle_list[idx][0]), 2, (255, 255, 0), -1)
+        for point in particle_list[idx][1]:
+            img = cv2.circle(img, tuple(point), 2, (0, 0, 255), -1)
+
+        print(f'\nloc: {particle_poses[idx]}\tmaxscore: {max_score}\tM: {self.M}')
+
+        self.images['particles'] = img
+
+        return np.array(norm_scores)
