3919be4355440866d7c069105595b8f1fdd1b8bf
Pawat <p.amonpitakpun@gmail.com>
2021-04-19 17:39:40 +0700

    convert to OOP

--- src/localization/scripts/run_localization.py
+++ src/localization/scripts/run_localization.py
@@ -7,7 +7,9 @@ import os
 import pprint
 import rospkg
 import rospy
+import sched
 import sys
+import time
 import traceback
 
 from cv_bridge import CvBridge, CvBridgeError
@@ -17,199 +19,219 @@ from std_msgs.msg import String, Float32MultiArray
 from sensor_msgs.msg import Image, LaserScan
 from threading import Lock, Thread
 
+from robot_state.srv import Localization, LocalizationResponse
+
 from modules.PF import ParticleFilter
 
 
 PACKAGE_PATH = rospkg.RosPack().get_path('localization')
 
-mutex = Lock()
 
-odom_buffer = [0, 0, 0, 0, 0]
-odom_msg_prev = [0, 0, 0, 0, 0]
-odom_last_update = datetime.now()
-odom_init = False
+class LocalizationNode():
+    def __init__(self):
+        # Subscribers
+        self.odom_sub = rospy.Subscriber(
+            'odomData', Float32MultiArray, self.odom_callback)
+        self.scanner_sub = rospy.Subscriber(
+            'scannerData', Float32MultiArray, self.scanner_callback)
 
-scanner_buffer = [0, 0, 0, 0]
-scanner_last_update = datetime.now()
+        # Publisher
+        self.pose_pub = rospy.Publisher(
+            'pose', Float32MultiArray, queue_size=5)
+        self.map_pub = rospy.Publisher('map', Image, queue_size=5)
 
-last_odom = None
-last_update = None
+        # Services
+        self.map_srv = rospy.Service(
+            'localization', Localization, self.localization_handler)
 
-pf = ParticleFilter(N=100)
+        self.cvBridge = CvBridge()
+        self.mutex = Lock()
 
-real_pose = [0, 0, 0]
-real_pose_origin = [0, 0, 0]
-real_pose_init = False
+        self.odom_buffer = [0, 0, 0, 0, 0]
+        self.odom_msg_prev = [0, 0, 0, 0, 0]
+        self.odom_last_update = datetime.now()
+        self.odom_init = False
 
-def odom_callback(msg):
-    global mutex
-    global odom_buffer, odom_init, odom_last_update, odom_msg_prev
-    global real_pose, real_pose_origin, real_pose_init
+        self.scanner_buffer = [0, 0, 0, 0]
+        self.scanner_last_update = datetime.now()
 
-    odom_msg = list(msg.data)
+        self.odom_last_calculate = datetime.now()
+        self.scan_last_calculate = datetime.now()
 
-    mutex.acquire()
-    now = datetime.now()
-    # print('odom', msg.data[3: 5])
-    if odom_init:
-        odom_buffer[0: 2] = odom_msg[0: 2]
-        odom_buffer[2] = odom_msg[2] * np.pi / 180
-        odom_buffer[3] += odom_msg[3] - odom_msg_prev[3]
-        odom_buffer[4] += odom_msg[4] - odom_msg_prev[4]
-    odom_msg_prev = odom_msg.copy()
-    # print(odom_buffer)
-    odom_init = True
-    odom_last_update = now
+        self.last_odom = None
+        self.last_update = None
 
-    if real_pose_init:
-        real_pose = [odom_msg[-2] / 100 - real_pose_origin[0], odom_msg[-1] / 100 - real_pose_origin[1], odom_buffer[2] - real_pose_origin[2]]
-    else:
-        real_pose_origin = [odom_msg[-2] / 100, odom_msg[-1] / 100, odom_buffer[2]]
-        real_pose_init = True
+        self.pf = ParticleFilter(N=100)
 
-    mutex.release()
+        self.real_pose = [0, 0, 0]
+        self.real_pose_origin = [0, 0, 0]
+        self.real_pose_init = False
 
+    def begin(self):
+        saved_maps = glob.glob(PACKAGE_PATH + '/../../saves/*.grid.npy')
+        saved_config = glob.glob(PACKAGE_PATH + '/../../saves/*.config.json')
 
-def scanner_callback(msg):
-    global scanner_buffer, scanner_last_update
+        for i, x in enumerate(saved_maps):
+            print(i, ':', x)
+        map_idx = int(input('choose saved map: ').strip())
 
-    mutex.acquire()
-    now = datetime.now()
-    # print('scanner', len(msg.data), now)
-    scanner_buffer = []
-    n = len(msg.data)
-    for i in range(n // 3):
-        point = list(msg.data[3 * i: 3 * (i + 1)])
-        # point[2] *= -1
-        scanner_buffer.append(point)
-    scanner_last_update = now
-    mutex.release()
+        for i,  x in enumerate(saved_config):
+            print(i, ':', x)
+        config_idx = int(input('choose saved map config: ').strip())
 
+        self.pf.init(ref_map_path=saved_maps[map_idx],
+                     ref_map_config_path=saved_config[config_idx])
 
-def process_data(odom_data, scanner_data, last_odom):
+        # rate = rospy.Rate(10)
+        delay_ms = 50
 
-    new_scan = np.array(scanner_data)[:, 1: 3]
+        self.odom_last_calculate = datetime.now()
+        self.scan_last_calculate = datetime.now()
 
-    dl = odom_data[3]
-    dr = odom_data[4]
+        self.timer = rospy.Timer(rospy.Duration(secs=0, nsecs=delay_ms * 1000),
+                                 self.timer_callbak)
 
-    c = 55.5
-    v_ = c * (dl + dr) / 2
+    def odom_callback(self, msg):
+        odom_msg = list(msg.data)
 
-    transform = [0, 0, 0]
-    if last_odom is not None:
-        theta = odom_data[2]
-        dtheta = (theta - last_odom[2])
+        self.mutex.acquire()
+        now = datetime.now()
+        # print('odom', msg.data[3: 5])
+        if self.odom_init:
+            self.odom_buffer[0: 2] = odom_msg[0: 2]
+            self.odom_buffer[2] = odom_msg[2] * np.pi / 180
+            self.odom_buffer[3] += odom_msg[3] - self.odom_msg_prev[3]
+            self.odom_buffer[4] += odom_msg[4] - self.odom_msg_prev[4]
+        self.odom_msg_prev = odom_msg.copy()
+        # print(self.odom_buffer)
+        self.odom_init = True
+        self.odom_last_update = now
 
-        transform[1] = v_ * np.sin(theta)
-        transform[0] = v_ * np.cos(theta)
-        transform[2] = dtheta
+        if self.real_pose_init:
+            self.real_pose = [odom_msg[-2] / 100 - self.real_pose_origin[0], odom_msg[-1] /
+                              100 - self.real_pose_origin[1], self.odom_buffer[2] - self.real_pose_origin[2]]
+        else:
+            self.real_pose_origin = [odom_msg[-2] / 100,
+                                     odom_msg[-1] / 100, self.odom_buffer[2]]
+            self.real_pose_init = True
 
-    return np.array(transform), new_scan, odom_data.copy()
+        self.mutex.release()
 
+    def scanner_callback(self, msg):
+        self.mutex.acquire()
+        now = datetime.now()
+        # print('scanner', len(msg.data), now)
+        self.scanner_buffer = []
+        n = len(msg.data)
+        for i in range(n // 3):
+            point = list(msg.data[3 * i: 3 * (i + 1)])
+            # point[2] *= -1
+            self.scanner_buffer.append(point)
+        self.scanner_last_update = now
+        self.mutex.release()
 
-def main():
-    cvBridge = CvBridge()
+    def localization_handler(self, req):
+        img = self.cvBridge.cv2_to_imgmsg(self.pf.getMap())
+        grid_pos = self.pf.getCell()
+        pos = self.pf.getPose()
+        pos[2] = (pos[2] * 180 / np.pi + 360) % 360
 
-    rospy.init_node('Localization', anonymous=True, log_level=rospy.INFO)
+        return LocalizationResponse(img, grid_pos, pos)
 
-    # Subscribers
-    odom_sub = rospy.Subscriber('odomData', Float32MultiArray, odom_callback)
-    scanner_sub = rospy.Subscriber(
-        'scannerData', Float32MultiArray, scanner_callback)
+    def process_data(self, odom_data, scanner_data, last_odom):
 
-    # Publisher
-    pose_pub = rospy.Publisher('pose', Float32MultiArray, queue_size=5)
-    map_pub = rospy.Publisher('map', Image, queue_size=5)
+        new_scan = np.array(scanner_data)[:, 1: 3]
 
-    global delay_ms, last_update
-    global odom_buffer, odom_last_update, last_odom, real_pose
-    global scanner_buffer, scanner_last_update
+        dl = odom_data[3]
+        dr = odom_data[4]
 
-    odom_last_calculate = datetime.now()
-    scan_last_calculate = datetime.now()
+        c = 55.5
+        v_ = c * (dl + dr) / 2
 
-    saved_maps = glob.glob(PACKAGE_PATH + '/../../saves/*.grid.npy')
-    saved_config = glob.glob(PACKAGE_PATH + '/../../saves/*.config.json')
+        transform = [0, 0, 0]
+        if self.last_odom is not None:
+            theta = odom_data[2]
+            dtheta = (theta - self.last_odom[2])
 
-    for i, x in enumerate(saved_maps):
-        print(i, ':', x)
-    map_idx = int(input('choose saved map: ').strip())
+            transform[1] = v_ * np.sin(theta)
+            transform[0] = v_ * np.cos(theta)
+            transform[2] = dtheta
 
-    for i,  x in enumerate(saved_config):
-        print(i, ':', x)
-    config_idx = int(input('choose saved map config: ').strip())
+        return np.array(transform), new_scan, odom_data.copy()
 
-    pf.init(ref_map_path=saved_maps[map_idx],
-            ref_map_config_path=saved_config[config_idx])
-
-    # rate = rospy.Rate(10)
-    delay_ms = 10
-
-    while not rospy.is_shutdown():
+    def timer_callbak(self, timer):
         try:
             now = datetime.now()
-            if last_update is None or (now - last_update).total_seconds() * 1e+3 >= delay_ms:
-                if odom_last_update > odom_last_calculate and \
-                        scanner_last_update > scan_last_calculate:
+            if self.odom_last_update > self.odom_last_calculate and \
+                    self.scanner_last_update > self.scan_last_calculate:
 
-                    mutex.acquire()
-                    odom_data = odom_buffer.copy()
-                    scanner_data = scanner_buffer.copy()
+                self.mutex.acquire()
+                odom_data = self.odom_buffer.copy()
+                scanner_data = self.scanner_buffer.copy()
 
-                    odom_buffer[3] = 0
-                    odom_buffer[4] = 0
+                self.odom_buffer[3] = 0
+                self.odom_buffer[4] = 0
 
-                    odom_last_calculate = now
-                    scan_last_calculate = now
-                    last_update = now
-                    mutex.release()
+                self.odom_last_calculate = now
+                self.scan_last_calculate = now
+                self.last_update = now
+                self.mutex.release()
 
-                    transform, new_scan, last_odom = process_data(
-                        odom_data, scanner_data, last_odom)
-                    pf.update(transform, new_scan)
-                    # print('PF updated with \tT:', transform)
-                    # print('loc', pf.getLoc())
+                transform, new_scan, self.last_odom = self.process_data(
+                    odom_data, scanner_data, self.last_odom)
+                self.pf.update(transform, new_scan)
+                # print('PF updated with \tT:', transform)
+                # print('loc', self.pf.getLoc())
 
-                    scan = np.zeros((500, 500, 3), dtype=np.uint8)
-                    scale = 50
-                    scan = cv2.circle(scan, (250, 250), 5, (100, 250, 50), -1)
-                    for p in scanner_buffer:
-                        scan = cv2.circle(
-                            scan, (int(250 - p[2] * scale), int(250 - p[1] * scale)), 2, (0, 125, 255))
+                scan = np.zeros((500, 500, 3), dtype=np.uint8)
+                scale = 50
+                scan = cv2.circle(scan, (250, 250), 5, (100, 250, 50), -1)
+                for p in self.scanner_buffer:
+                    scan = cv2.circle(
+                        scan, (int(250 - p[2] * scale), int(250 - p[1] * scale)), 2, (0, 125, 255))
 
-                    cv2.imshow('scan', scan)
-                    name, img = pf.getImage()
-                    if img is not None:
-                        cv2.imshow(name, img)
-                    cv2.waitKey(1)
+                cv2.imshow('scan', scan)
+                name, img = self.pf.getImage()
+                if img is not None:
+                    cv2.imshow(name, img)
+                cv2.waitKey(1)
 
-                    last_update = datetime.now()
+                self.last_update = datetime.now()
 
-                    pose = pf.getPose()
-                    cell = pf.getCell()
-                    print(f'\nloc: {pose[: -1]}, dir: {(pose[-1] * 180 / np.pi + 360) % 360}')
-                    print(f'cell: {cell}')
+                pose = self.pf.getPose()
+                cell = self.pf.getCell()
+                print(
+                    f'\nloc: {pose[: -1]}, dir: {(pose[-1] * 180 / np.pi + 360) % 360}')
+                print(f'cell: {cell}')
 
-                    # Publish
-                    try:
-                        pose_msg = Float32MultiArray(data=pf.getPose())
-                        pose_pub.publish(pose_msg)
-                    except CvBridgeError as e:
-                        rospy.logerr(e)
+                # Publish
+                try:
+                    pose_msg = Float32MultiArray(data=self.pf.getPose())
+                    self.pose_pub.publish(pose_msg)
+                except self.cvBridgeError as e:
+                    rospy.logerr(e)
 
-                    try:
-                        map_msg = cvBridge.cv2_to_imgmsg(pf.getMap())
-                        map_pub.publish(map_msg)
-                    except CvBridgeError as e:
-                        rospy.logerr(e)
-                    
+                try:
+                    map_msg = self.cvBridge.cv2_to_imgmsg(self.pf.getMap())
+                    self.map_pub.publish(map_msg)
+                except self.cvBridgeError as e:
+                    rospy.logerr(e)
+
         except Exception as e:
             print('  ERROR: ', e)
             traceback.print_exc()
 
-        # loc_pub.publish()
-        # rate.sleep()
+
+def main():
+
+    rospy.init_node('Localization', anonymous=True, log_level=rospy.INFO)
+
+    try:
+        node = LocalizationNode()
+        node.begin()
+        rospy.spin()
+    except rospy.ROSInterruptException as e:
+        print(e)
 
 
 if __name__ == '__main__':
