1f96a6e8a4ec4733691df1d3b5c48b62e0845b56
Pawat Amonpitakpun <p.amonpitakpun@gmail.com>
2021-05-15 21:07:21 +0700

    fix image_to_map.py

--- src/image_to_np.py
+++ src/image_to_np.py
@@ -4,6 +4,10 @@ import numpy as np
 import os
 
 
+def logOdd(x):
+    y = (1 - x) * (x > 0) + 0.0001
+    return np.log(x / y + 0.0001)
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 IMAGE_FOLDER_PATH = dir_path + '/../images/'
 IMAGE_FILE_REGEX = IMAGE_FOLDER_PATH + '*.png'
@@ -15,6 +19,12 @@ for i, path in enumerate(paths):
 idx = int(input('please select a file: ').strip())
 
 img = cv2.imread(paths[idx])
+img = 255 - cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# cv2.imshow('reverted', img)
+# cv2.waitKey(0)
+
+grid = logOdd(img / 255)
 
 filename = '_'.join(input('please enter filename: ').strip().split())
-np.save(MAP_FOLDER_PATH + filename, img)
+np.save(MAP_FOLDER_PATH + filename, grid)
