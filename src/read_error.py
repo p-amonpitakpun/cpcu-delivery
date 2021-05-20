3efb38876211bb7319c33e7edb583d5e02d85180
Pawat <p.amonpitakpun@gmail.com>
2021-05-20 18:24:40 +0700

    change radian to degree in orientation error

--- src/read_error.py
+++ src/read_error.py
@@ -81,23 +81,29 @@ if len(paths) > 0:
         T.append((datetime.fromtimestamp(t) - start_time).total_seconds())
         X.append(x)
         Y.append(y)
-        z = np.fmod(z, np.pi * 2)
-        Z.append(z if z < np.pi else np.pi - z)
+        z = np.fmod(np.rad2deg(z), 360)
+        Z.append(z if z < 180 else z - 360)
 
-    fig1, axs = plt.subplots(2, 1, sharex=True, figsize=(10, 20))
+    fig1, axs = plt.subplots(2, 1, sharex=True, figsize=(10, 10))
     fig1.suptitle(f'Localization Error of Test case {log_time}')
     axs[0].plot(T, X, c='b', label='dx')
     axs[0].plot(T, Y, c='r', label='dy')
-    axs[1].plot(T, Z, c='k', label='dTheta', yunits=radians)
+    axs[1].plot(T, Z, c='k', label='dTheta')
 
-    axs[1].set_ylim(- np.pi, np.pi)
+    axs[1].set_ylim(-20, 20)
 
     axs[0].set_ylabel('Position Error (m)')
-    axs[1].set_ylabel('Orientation Error (rad)')
+    axs[1].set_ylabel('Orientation Error (degree)')
     axs[1].set_xlabel('Time (s)')
 
     axs[0].legend(loc='upper left')
     axs[1].legend(loc='upper left')
     
     fig1.savefig(dir_path + f'/../images/error_{timestamp}.png')
+
+    # fig, axt = plt.subplots()
+    # axt.scatter(X, Y, s=1)
+    # axt.set_xlim(-0.20, 0.20)
+    # axt.set_ylim(-0.20, 0.20)
+    
     plt.show()
